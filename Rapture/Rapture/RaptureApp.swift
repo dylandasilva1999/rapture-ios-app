80b74763b6c2d2ad8a0af6855101743eac1c332e
Dylan da Silva <dylandasilva.b@gmail.com>
2021-09-22 18:21:22 +0200

    Initialise Firebase In App

--- Rapture/Rapture/RaptureApp.swift
+++ Rapture/Rapture/RaptureApp.swift
@@ -10,9 +10,21 @@ import Firebase
 
 @main
 struct RaptureApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+//App Delegate Class
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Firebase Initialised...")
+        FirebaseApp.configure()
+        return true
     }
 }
