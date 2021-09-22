8358cfa608954279f67d5a897efcb3297a10e6de
Dylan da Silva <dylandasilva.b@gmail.com>
2021-09-22 17:50:35 +0200

    Add Project Files To GitHub Folder

---
+++ Rapture/Rapture/RaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaptureApp.swift
+//  Rapture
+//
+//  Created by Dylan da Silva on 2021/09/22.
+//
+
+import SwiftUI
+
+@main
+struct RaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
