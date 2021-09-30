17316c5c1172da77b6f7911b336bc61ef5c25597
Dylan da Silva <dylandasilva.b@gmail.com>
2021-09-30 20:00:26 +0200

    Implement Sign Up UI

--- Rapture/Rapture/View/SignUpView/SignUpView.swift
+++ Rapture/Rapture/View/SignUpView/SignUpView.swift
@@ -1,5 +1,5 @@
 //
-//  SignInView.swift
+//  SignUpView.swift
 //  Rapture
 //
 //  Created by Dylan da Silva on 2021/09/30.
@@ -7,12 +7,14 @@
 
 import SwiftUI
 
-struct SignInView: View {
+struct SignUpView: View {
     
     @State var color = Color("Red")
     @State var email = ""
     @State var password = ""
+    @State var retypePassword = ""
     @State var visible = false
+    @State var retypeVisible = false
     
     var body: some View {
         ZStack(alignment: .topTrailing) {
@@ -24,34 +26,34 @@ struct SignInView: View {
                 VStack {
                     
                     HStack {
-                        Spacer()
-                        
                         //Register button
                         Button(action: {
                             
                         }) {
-                            Text("register")
-                                .font(Font.custom("Gilroy-SemiBold", size: 22))
+                            Image(systemName: "chevron.left")
+                                .font(.title)
+                                .foregroundColor(.white)
+                            
+                            Text("back")
+                                .font(Font.custom("Gilroy-Regular", size: 22))
                                 .foregroundColor(.white)
                         }
                         .padding(.top, 20)
+                        
+                        Spacer()
                     }
                     
-                    //Sign In Logo
+                    //Sign Up Logo
                     Image("Splash Logo")
                         .resizable()
                         .renderingMode(.original)
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 240, height: 240, alignment: .center)
-                        .padding(.top, 60)
+                        .padding(.top, 40)
                     
                     VStack(alignment: .leading, spacing: 6) {
-                        //Sign welcome text
-                        Text("welcome gamer!")
-                            .font(Font.custom("Gilroy-Regular", size: 22))
-                            .foregroundColor(.white)
-                        //Sign In boilerplate text
-                        Text("Sign in to your account.")
+                        //Sign Up boilerplate text
+                        Text("Create your account.")
                             .font(Font.custom("Gilroy-Bold", size: 30))
                             .foregroundColor(.white)
                         
@@ -96,25 +98,42 @@ struct SignInView: View {
                         .background(RoundedRectangle(cornerRadius: 12).stroke(self.password != "" ? Color("Red") : self.color, lineWidth: 4))
                         .padding(.top, 20)
                         
-                        //Forget password text
-                        HStack {
-                            Spacer()
+                        HStack(spacing: 15) {
+                            VStack {
+                                if self.visible {
+                                    //Re-enter Password input field
+                                    TextField("re-enter password", text: self.$retypePassword)
+                                        .font(Font.custom("Gilroy-Regular", size: 20))
+                                        .foregroundColor(.white)
+                                        .foregroundColor(.white)
+                                        .preferredColorScheme(.dark)
+                                } else  {
+                                    SecureField("re-enter password", text: self.$retypePassword)
+                                        .font(Font.custom("Gilroy-Regular", size: 20))
+                                        .foregroundColor(.white)
+                                        .foregroundColor(.white)
+                                        .preferredColorScheme(.dark)
+                                }
+                            }
                             
-                            Button(action: {
-                                
+                            //Show re-enter password icon
+                            Button (action: {
+                                self.retypeVisible.toggle()
                             }) {
-                                Text("forget password")
-                                    .font(Font.custom("Gilroy-Regular", size: 22))
+                                Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
                                     .foregroundColor(.white)
                             }
+                                
                         }
+                        .padding(20)
+                        .background(RoundedRectangle(cornerRadius: 12).stroke(self.retypePassword != "" ? Color("Red") : self.color, lineWidth: 4))
                         .padding(.top, 20)
                         
-                        //Sign In Button
+                        //Sign Up Button
                         Button(action: {
                             
                         }) {
-                            Text("sign in")
+                            Text("sign up")
                                 .font(Font.custom("Gilroy-SemiBold", size: 22))
                                 .foregroundColor(.white)
                                 .padding(.vertical, 25)
@@ -133,8 +152,8 @@ struct SignInView: View {
     }
 }
 
-struct SignInView_Previews: PreviewProvider {
+struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
-        SignInView()
+        SignUpView()
     }
 }
