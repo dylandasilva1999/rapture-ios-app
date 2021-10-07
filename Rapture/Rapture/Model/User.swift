42f3d117b26f51f63735ce03e9834fd68d104a38
Dylan da Silva <dylandasilva.b@gmail.com>
2021-10-07 14:06:23 +0200

    Implement Extensions & AuthService, StorageService

---
+++ Rapture/Rapture/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Rapture
+//
+//  Created by Dylan da Silva on 2021/10/07.
+//
+
+import Foundation
+import UIKit
+
+struct User {
+    var uid: String
+    var email: String
+    var profileImageUrl: String
+    var username: String
+    var searchName: [String]
+    var bio: String
+}
