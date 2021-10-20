4bbb4271a8dfe86abfb95d998cb64909afe91662
Dylan da Silva <dylandasilva.b@gmail.com>
2021-10-20 20:02:19 +0200

    Add User Own Posts To Profile

--- Rapture/Rapture/View/ProfileView/ProfileView.swift
+++ Rapture/Rapture/View/ProfileView/ProfileView.swift
@@ -7,17 +7,46 @@
 
 import SwiftUI
 import SDWebImageSwiftUI
+import FirebaseAuth
 
 struct ProfileView: View {
     
     @EnvironmentObject var session: SessionStore
     @State private var selection = 1
+    @StateObject var profileService = ProfileService()
+    
+    var columns = [GridItem(.flexible(minimum: 120), spacing: 0),
+                   GridItem(.flexible(minimum: 120), spacing: 0)]
     
     var body: some View {
         ScrollView {
             VStack {
                 ProfileHeader(user: self.session.session)
             }
+            .frame(width: UIScreen.main.bounds.width)
+            .padding(.horizontal)
+            
+            VStack {
+                if selection  == 1 {
+                    LazyVGrid(columns: columns, alignment: .center, spacing: 0) {
+                        ForEach(self.profileService.posts, id: \.postId) {
+                            (post) in
+                            
+                            WebImage(url: URL(string: post.mediaUrl))
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: UIScreen.main.bounds.width/2.5, height: UIScreen.main.bounds.height/4)
+                                .cornerRadius(20)
+                                .padding(.bottom, 22)
+                        }
+                    }
+                }
+            }
+            .frame(width: UIScreen.main.bounds.width/1.1)
+            .padding(.top, 20)
+        }
+        .onAppear {
+            self.profileService.loadUserPosts(userId: Auth.auth().currentUser!.uid)
         }
     }
 }
